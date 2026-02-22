abbb59525aa0fc2fe012f80b7088d8e29685f08c
Abhishek Kumar <akkr0077@gmail.com>
2026-02-22 16:46:41 +0530

    🎉Added Product Microservice base code line with mySql db

---
+++ eCommerceSolution.ProductsService/ProductsMicroService.API/Program.cs
@@ -0,0 +1,33 @@
+using eCommerce.ProductsMicroService.API.Middleware;
+using eCommerce.ProductsService.BusinessLogicLayer;
+using eCommerce.ProductsService.DataAccessLayer;
+using FluentValidation.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add DAL and BLL services
+builder.Services.AddDataAccessLayer(builder.Configuration);
+builder.Services.AddBusinessLogicLayer();
+
+
+builder.Services.AddControllers();
+
+//FluentValidations
+builder.Services.AddFluentValidationAutoValidation();
+
+
+var app = builder.Build();
+
+app.UseExceptionHandlingMiddleware();
+app.UseRouting();
+
+//Auth
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+//app.MapGet("/", () => "Hello World!");
+
+app.Run();
